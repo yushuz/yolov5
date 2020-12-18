31ae8aae797406ce78dc718c31f2d2492cbe9eed
Yushu Zhang <yushuz@umich.edu>
2020-12-18 16:59:56 -0500

    renew

--- detect.py
+++ detect.py
@@ -117,7 +117,7 @@ def detect(save_img=False):
             # Stream results
             if view_img:
                 cv2.imshow(str(p), im0)
-                if cv2.waitKey(1) == ord('q'):  # q to quit
+                if cv2.waitKey(100) == ord('q'):  # q to quit
                     raise StopIteration
 
             # Save results (image with detections)
